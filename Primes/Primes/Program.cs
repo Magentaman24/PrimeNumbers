c33098727dc8fd7d3541dc1fc6a908741742213f
Dom R <ramutd@outlook.com>
2021-09-10 11:03:04 -0400

    Added initial project

---
+++ Primes/Primes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
